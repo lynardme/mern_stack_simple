738c5cfb20c7128651ffdd17ef921b935717ef07
lynard <lynardsalingujay@outlook.com>
2020-05-26 10:56:36 +1200

    add rest api

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
+const items = require("./routes/api/items");
+
 const app = express();
 
 // bodyparser middleware
@@ -14,6 +16,9 @@ mongoose
   .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Mongo DB connected"))
   .catch((err) => console.log(err));
+
+// Use routes
+app.use("/api/items", items);
 
 const port = process.env.PORT || 5000;
 
